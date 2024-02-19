1682e2a495a68dc8bb1b5e2e3727dbb57107a677
reed <reed@pogi.com>
2024-02-19 11:21:51 +0800

    add link for online db

--- server/server.js
+++ server/server.js
@@ -15,12 +15,15 @@ app.use(cors());
 app.use(express.json());
 
 // db setup
+// const db = new pg.Client({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DB_CONNECTION_STRING,
 });
 db.connect();
 
